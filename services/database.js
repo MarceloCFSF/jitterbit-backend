8aaf820649f71d481ea882efa64b7496d72da84a
MarceloCFSF <marcelo.cfsf@gmail.com>
2024-06-23 18:06:38 -0300

    configure database connection

---
+++ services/database.js
@@ -0,0 +1,13 @@
+import pg from 'pg';
+import 'dotenv/config';
+const { Pool } = pg;
+
+const database = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port:process.env.DB_PORT 
+});
+
+export default database;
